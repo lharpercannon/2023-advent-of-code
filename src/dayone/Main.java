e14c621957111e579447f4bf7a2e12119e84a92a
Lindsay Hodgens <31974091+lharpercannon@users.noreply.github.com>
2023-12-05 20:38:01 -0500

    Add comments outlining possible plan of action

--- src/dayone/Main.java
+++ src/dayone/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Read in contents of text file, split at the line break and put each line into an array
+
+        // Create a hashmap with key value pairs of numbers one through ten spelled out (keys) and
+        // numeric versions (values)
+
+        // use a loop/string api to convert each item in the array into lowercase
+
+        // Use substring , regex, whatever to look from the left and find the first number and look from
+        // the right and find the first number
     }
 }
