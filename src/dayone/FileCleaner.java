b54a89bf35777ab65e3a9e29571de87b468e9576
Lindsay Hodgens <31974091+lharpercannon@users.noreply.github.com>
2023-12-05 21:20:59 -0500

    Start drafting convertLinesToLowercase()

--- src/dayone/FileCleaner.java
+++ src/dayone/FileCleaner.java
@@ -19,13 +19,20 @@ public class FileCleaner {
     }
 
     public ArrayList<String> convertTextFileToArray(FileReader input) throws FileNotFoundException {
-
         Scanner inputScanner = new Scanner(input);
         while (inputScanner.hasNextLine()) {
             String line = inputScanner.nextLine();
             inputLines.add(line);
         }
-
         return inputLines;
     }
+
+    public void convertLinesToLowercase() {
+        ArrayList<String> lowercaseLines = new ArrayList<>();
+        lowercaseLines = inputLines.stream().filter(inputLines -> {
+            inputLines.toLowerCase()
+        });
+    }
+
+
 }
