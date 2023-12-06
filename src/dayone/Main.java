3210732c50ab80a506f3ca9991ca950aeae0ae75
Lindsay Hodgens <31974091+lharpercannon@users.noreply.github.com>
2023-12-05 20:56:07 -0500

    Delete extra junk from Main

--- src/dayone/Main.java
+++ src/dayone/Main.java
@@ -1,10 +1,15 @@
 package dayone;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         // Read in contents of text file, split at the line break and put each line into an array
+
 
         // Create a hashmap with key value pairs of numbers one through ten spelled out (keys) and
         // numeric versions (values)
@@ -13,5 +18,7 @@ public class Main {
 
         // Use substring , regex, whatever to look from the left and find the first number and look from
         // the right and find the first number
+
+
     }
 }
