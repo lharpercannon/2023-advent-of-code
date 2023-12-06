adf079c0be5b724a8b589851f44b482c25dd5b8e
Lindsay Hodgens <31974091+lharpercannon@users.noreply.github.com>
2023-12-05 21:13:00 -0500

    Draft passing test for convertTextFileToArray()

--- test/dayone/FileCleanerTest.java
+++ test/dayone/FileCleanerTest.java
@@ -1,12 +1,15 @@
 package dayone;
+import dayone.FileCleaner;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
@@ -18,5 +21,11 @@ public class FileCleanerTest {
         assertNotNull(input);
     }
 
-
+    @Test
+    void itCanReadEachInputLineIntoAnArray() throws FileNotFoundException {
+        FileCleaner fileCleaner = new FileCleaner();
+        FileReader testInput = new FileReader("./testinput.txt");
+        ArrayList<String> inputLines = fileCleaner.convertTextFileToArray(testInput);
+        assertEquals(4, inputLines.size());
+    }
 }
