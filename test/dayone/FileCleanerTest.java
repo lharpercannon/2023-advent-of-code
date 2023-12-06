49447b42764f3b0e428b3ed9ad7a062358d7a9c3
Lindsay Hodgens <31974091+lharpercannon@users.noreply.github.com>
2023-12-05 21:42:44 -0500

    Write passing test for convertLinesToLowercase() method

--- test/dayone/FileCleanerTest.java
+++ test/dayone/FileCleanerTest.java
@@ -7,13 +7,15 @@ import org.junit.jupiter.api.Assertions.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class FileCleanerTest {
+
+    List<String> linesToLowercase = new ArrayList<>();
 
     @Test
     void itCanLocateCorrectFile() throws FileNotFoundException {
@@ -27,5 +29,15 @@ public class FileCleanerTest {
         FileReader testInput = new FileReader("./testinput.txt");
         ArrayList<String> inputLines = fileCleaner.convertTextFileToArray(testInput);
         assertEquals(4, inputLines.size());
+    }
+
+    @Test
+    void itCanConvertLinesToLowercase() {
+        FileCleaner fileCleaner = new FileCleaner();
+        linesToLowercase.add("AKsjjlkA");
+        linesToLowercase.add("AAA");
+        List<String> lowercaseLines = fileCleaner.convertLinesToLowercase(linesToLowercase);
+        assertTrue(lowercaseLines.contains("aksjjlka"));
+        assertTrue(lowercaseLines.contains("aaa"));
     }
 }
