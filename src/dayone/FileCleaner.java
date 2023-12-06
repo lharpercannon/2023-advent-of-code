31d1967dc7de480da6dc23a65b720fa7c0bd770e
Lindsay Hodgens <31974091+lharpercannon@users.noreply.github.com>
2023-12-05 21:02:23 -0500

    Draft convertTextFileToArray() method

--- src/dayone/FileCleaner.java
+++ src/dayone/FileCleaner.java
@@ -2,17 +2,26 @@ package dayone;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileCleaner {
 
     private FileReader input;
+    ArrayList<String> inputLines = new ArrayList<>();
+
     public void readInTextFile() throws FileNotFoundException {
         input = new FileReader("./dayoneinput.txt");
     }
 
-    public void convertTextFileToArray() throws FileNotFoundException {
+    public ArrayList<String> convertTextFileToArray() throws FileNotFoundException {
 
-        Scanner scanner = new Scanner(input);
+        Scanner inputScanner = new Scanner(input);
+        while (inputScanner.hasNextLine()) {
+            String line = inputScanner.nextLine();
+            inputLines.add(line);
+        }
+
+        return inputLines;
     }
 }
