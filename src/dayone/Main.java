c670515638252934e1a4fce7f2d5245a47472c9f
Lindsay Hodgens <31974091+lharpercannon@users.noreply.github.com>
2023-12-06 20:57:32 -0500

    Successful run--Main produces the right value to move to the next puzzle

--- src/dayone/Main.java
+++ src/dayone/Main.java
@@ -2,22 +2,21 @@ package dayone;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        // Read in contents of text file, split at the line break and put each line into an array
-
-
-        // Create a hashmap with key value pairs of numbers one through ten spelled out (keys) and
-        // numeric versions (values)
-
-        // use a loop/string api to convert each item in the array into lowercase
-
-        // Use substring , regex, whatever to look from the left and find the first number and look from
-        // the right and find the first number
+        FileCleaner fileCleaner = new FileCleaner();
+        FileReader input = new FileReader("./dayoneinput.txt");
+        List<String> inputStepTwo = fileCleaner.convertTextFileToArray(input);
+        List<String> inputStepThree = fileCleaner.convertLinesToLowercase(inputStepTwo);
+        List<Integer> inputStepFour = fileCleaner.removeLettersFromLines(inputStepThree);
+        List<Integer> inputStepFive = fileCleaner.reduceToFirstAndLastNumbers(inputStepFour);
+        Integer result = fileCleaner.sumArrayElements(inputStepFive);
+        System.out.println("The total is: " + result);
 
 
     }
